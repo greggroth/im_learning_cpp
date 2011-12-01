b4d112502b31085e9b29a02e5c47277378264e7c
Greggory Rothmeier <greggroth@gmail.com>
2011-12-01 17:09:01 -0500

    added comments and removed unnecessary code

--- comp_programing_exam_solution/comp_exam.cpp
+++ comp_programing_exam_solution/comp_exam.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-#include <cmath>
+#include <stdlib.h>
 #include <sstream>
+// Using a high-precision library (~64 decimals of precision) to capture the difference between
+// the series and real solutions (http://crd.lbl.gov/~dhbailey/mpdist/qd-2.3.12.tar.gz)
 #include <qd/qd_real.h>
 
-using std::cout;
-using std::endl;
-using namespace std ;
+using namespace std;
 
-bool checkForDouble(string const& s) {
+bool checkForDouble(string s) {
   istringstream ss(s);
   double d;
   return (ss >> d) && (ss >> std::ws).eof();
@@ -33,17 +33,21 @@ int main (void) {
   double doubS;
   int intN;
   
+  // Get and verify a valid s
   while (1) {
     cout << "S (must be between 0 and 1):  ";
     getline(cin, inS);
     
+    // Make sure the string is a number
     if (!checkForDouble(inS)) {
       cout << "I need a number for this to work.  Try again." << endl;
       continue;
     }
-    stringstream strStream;
-    strStream << inS;
-    strStream >> doubS;
+    
+    // Convert the string to double
+    doubS = atof ( inS.c_str() );
+    
+    // Check that the number is between 0 and 1
     if (doubS < 0 || doubS > 1) {
       cout << "It must be between 0 and 1.  Try again." << endl;
       continue;
@@ -52,18 +56,21 @@ int main (void) {
     break;
   }
   
+  // Get and verify a valid n
   while (1) {
     cout << "N (must be an integer greater than 0):  ";
     getline(cin, inN);
     
+    // Makes sure it's an integer
     if (!checkForInteger(inN)) {
       cout << "I need an integer for this to work.  Keep trying." << endl;
       continue;
     }
     
-    stringstream strStream;
-    strStream << inN;
-    strStream >> intN;
+    // If it is an integer, convert the string to int
+    intN = atoi ( inN.c_str() );
+    
+    // Criticize the user if they don't read the instructions
     if (intN < 0) {
       cout << "It's not that hard.  Just enter a number larger than 0." << endl;
       continue;
@@ -73,14 +80,22 @@ int main (void) {
   }
   
   //  Now that we have good inputs for s and n, we can do some math
-  qd_real seriesValue = 0, realValue, percentError, qdS = doubS;
-  realValue = 1/(1-qdS);
   
+  // Using quad-double for extra precision
+  qd_real seriesValue = 0, realValue, percentError;
+  
+  // Get the exact solution
+  realValue = 1/(1-doubS);
+  
+  // Get the series solution
   for (int i = 0; i <= intN; i++) {
-    seriesValue += pow(qdS, i);
+    seriesValue += pow(doubS, i);
   }
+  
+  // Compare them
   percentError = ((abs (realValue - seriesValue))/realValue) * 100;
   
+  // Output the result
   cout.precision(64);
   cout << "Real Value:    " << realValue << endl;
   cout << "Series Value:  " << seriesValue << endl;
